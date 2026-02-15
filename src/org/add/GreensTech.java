ecde0447c5c93d81286eca0615147e21f1a55eee
yamunakaruna <yamunakaruna360@gmail.com>
2026-02-15 21:14:14 +0530

    conficts resolved

--- src/org/add/GreensTech.java
+++ src/org/add/GreensTech.java
@@ -5,19 +5,18 @@ public class GreensTech {
 	private void tc1() {
 		System.out.println("Name");
 	}
-<<<<<<< HEAD
-	private void tc3() {
-		System.out.println("Subject");
-=======
+
 	private void tc2() {
 		System.out.println("Class");
+	}
+
+	private void tc3() {
+		System.out.println("Subject");
 	}
 
 	private void tc4() {
 		System.out.println("Address");
 
->>>>>>> 07b3dd0e61189b8f06695ec7d569cf1903769f1f
 	}
-	
 
 }
